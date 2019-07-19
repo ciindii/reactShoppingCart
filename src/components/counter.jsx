264f2ae68dc9ed9c4b56efb54e40b61f39942381
Cindy Truong <cindythitruong@gmail.com>
2019-07-18 22:37:04 -0700

    styled btns

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   styles = {
@@ -11,13 +12,22 @@ class Counter extends Component {
   };
 
   render() {
+
     return (
       <React.Fragment>
-
-        <span style={this.styles}className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          { this.state.tags.map(tag => <li>{tag}</li>) }
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
