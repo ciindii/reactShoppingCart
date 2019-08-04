8e6955cb6eec05b8fe4c2f2e9c88a4b1ffdfec99
Cindy Truong <cindythitruong@gmail.com>
2019-08-03 22:48:32 -0700

    added player and header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,96 @@
 import React from 'react';
-import Counter from './components/counter';
+// import Player from './components/counter';
 import './App.css';
 
-class App extends React.Component {
+function Header(props) {
+  return (
+    <header>
+      <h1>{ props.title }</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+  );
+}
+
+const Player = (props) => {
+  return (
+    <div>
+      <span>{props.name}</span>
+      <Counter />
+    </div>
+  )
+}
+class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      score: 0
+    };
+  }
+
+  increnmentScore = () => {
+    this.setState( prevState => {
+      return {
+        score: prevState.score + 1
+      }
+    });
+  }
+
+  decrementScore = () => {
+    this.setState( prevState => {
+      return {
+        score: prevState.score - 1
+      }
+    });
+  }
+
   render() {
-    return(
-      <div className="App">
-        <header>
-          <h1>game scoreboard</h1>
-        </header>
-        <Counter />
+    return (
+      <div className="counter">
+        <button className="" onClick={this.increnmentScore}>+</button>
+        <span className="">{this.state.score}</span>
+        <button className="" onClick={this.decrementScore}>-</button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  state = {
+    players: [
+     {
+      name: "Guil",
+      score: 50,
+      id: 1
+     },
+      {
+      name: "Cindy",
+      score: 30,
+      id: 2
+     },
+      {
+      name: "Ashley",
+      score: 10,
+      id: 3
+     },
+      {
+      name: "James",
+      score: 40,
+      id: 4
+     }
+    ]
+  };
+  render(){
+    return (
+     <div className="scoreboard">
+        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+      {/* players list */}
+      {this.state.players.map( player =>
+          <Player
+            name={player.name}
+            id={player.id}
+           />
+      )}
       </div>
     );
   }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,38 +1,5 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {
-  constructor() {
-    super()
-    this.state = {
-      score: 0
-    };
-  }
 
-  increnmentScore = () => {
-    this.setState( prevState => {
-      return {
-        score: prevState.score + 1
-      }
-    });
-  }
-
-  decrementScore = () => {
-    this.setState( prevState => {
-      return {
-        score: prevState.score - 1
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <button className="" onClick={this.increnmentScore}>+</button>
-        <span className="">{this.state.score}</span>
-        <button className="" onClick={this.decrementScore}>-</button>
-      </div>
-    );
-  }
-}
 
 export default Counter;
